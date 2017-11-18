71e149d1fcaad859a9be274946c17fa927e6fa17
Bruno C. Couto <brunodev02221@gmail.com>
2017-11-18 14:32:03 -0200

    Update build process to improve dev workflow
    Improve the devServer message to include information about the machine
    host running the application.
    Support to HTTPS

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,20 +3,36 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const paths = require('./paths.js');
 
+const protocol = process.env.PROTOCOL || 'http';
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
+
 module.exports = merge(common, {
 
   devtool: 'inline-source-map',
 
   devServer: {
+    publicPath: '/',
     contentBase: paths.build,
     compress: true,
     hot: true,
-    useLocalIp: true,
-    host: '0.0.0.0',
-    port: 3000,
+    host: host,
+    port: port,
     clientLogLevel: 'none',
     noInfo: true,
     open: true,
+    quiet: true,
+    https: protocol === 'https',
+
+    after: () => {
+      console.log('Accessing the application');
+      console.log('=================================');
+      console.log('\nIn your machine:');
+      console.log(`${protocol}://${host}:${port}/`);
+      console.log(`${protocol}://localhost:${port}/`);
+      console.log('\nIn your network:')
+      console.log(`${protocol}://{youLocalExternalIp}:${port}`);
+    }
   },
 
   module: {
